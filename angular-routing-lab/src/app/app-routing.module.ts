54e2e9dde5d3d6f69f9dffb8f0546ed12d4085c5
UBSITSalango <20162396@s.ubaguio.edu>
2025-03-04 15:07:57 +0800

    Setting up the routes.

--- angular-routing-lab/src/app/app-routing.module.ts
+++ angular-routing-lab/src/app/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent }];
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes), CommonModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
