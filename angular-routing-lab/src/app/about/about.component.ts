3ba5242b6afdd12dcdf849ed996952ddd1f7dd39
UBSITSalango <20162396@s.ubaguio.edu>
2025-03-06 14:49:49 +0800

    Fixing child component navigation for better navigation.

--- angular-routing-lab/src/app/about/about.component.ts
+++ angular-routing-lab/src/app/about/about.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [RouterModule],
+  standalone: true,
+  imports: [RouterModule, CommonModule], 
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
+  styleUrls: ['./about.component.css'] 
 })
 export class AboutComponent {
+  isProfileOpen = false; // Track profile state
 
+  constructor(private router: Router, private route: ActivatedRoute) {}
+
+  openProfile() {
+    this.router.navigate(['profile'], { relativeTo: this.route }); // Open profile
+    this.isProfileOpen = true;
+  }
+
+  closeProfile() {
+    this.router.navigate(['/about']); // Close profile and go back to About
+    this.isProfileOpen = false;
+  }
 }
