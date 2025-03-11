0f1455d8b41a6730bce0712cdadb1fe2e75c938f
UBSITSalango <20162396@s.ubaguio.edu>
2025-03-11 16:09:56 +0800

    Updating the profile component and navbar for the dropdown menu.

--- angular-routing-lab/src/app/app.routes.ts
+++ angular-routing-lab/src/app/app.routes.ts
@@ -4,18 +4,13 @@ import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProfileComponent } from './users/profile/profile.component';
 import { AdminGuard } from './admin/admin.guard';
-import { UsersComponent } from './users/users.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },  // Redirect empty path to /home
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
-  { 
-    path: 'users', component: UsersComponent, children: [  // Moved profile under users correctly
-      { path: 'profile', component: ProfileComponent }
-    ] 
-  },
+  { path: 'profile', component: ProfileComponent },
   { 
     path: 'admin', 
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), 
--- angular-routing-lab/src/app/users/profile/profile.component.ts
+++ angular-routing-lab/src/app/users/profile/profile.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
-
+  userName: string = 'Tyrone Salango';
+  userRole: string = 'Full-Stack Developer';
+  userLocation: string = 'Baguio, Philippines';
+  userEmail: string = 'tyrone@example.com';
+  userContact: string = '0912-345-6789';
+  userImage: string = 'https://images.unsplash.com/photo-1557447928-9a1ca5819ed6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3';
 }
