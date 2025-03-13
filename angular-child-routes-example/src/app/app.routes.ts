71ea4c68665de4753a7ef8835a6920ed446db30e
UBSITSalango <20162396@s.ubaguio.edu>
2025-03-13 15:54:19 +0800

    Configuring routing to use AdminGuard.

--- angular-child-routes-example/src/app/app.routes.ts
+++ angular-child-routes-example/src/app/app.routes.ts
@@ -2,30 +2,29 @@ import { Routes } from '@angular/router';
 import { ParentComponent } from './parent/parent.component';
 import { ChildOneComponent } from './parent/child-one/child-one.component';
 import { ChildTwoComponent } from './parent/child-two/child-two.component';
+import { AdminGuard } from './admin.guard'; // Import AdminGuard
+
 export const routes: Routes = [
-    {
+  {
     path: 'parent',
     component: ParentComponent,
     children: [
-        {
-            path: 'child-one',
-            component: ChildOneComponent
-        },
-        {
-            path: 'child-two',
-            component: ChildTwoComponent
-        },
-        {
-            path: '',
-            redirectTo: 'child-one',
-            pathMatch: 'full'
-        }
+      { path: 'child-one', component: ChildOneComponent },
+      { path: 'child-two', component: ChildTwoComponent }
     ]
-    },
-    {
-        path: '',
-        redirectTo: '/parent',
-        pathMatch: 'full'
-
-    }
+  },
+  {
+    path: 'admin',
+    component: ParentComponent, // Use a proper AdminComponent if needed
+    canActivate: [AdminGuard],
+    children: [
+      { path: 'dashboard', component: ChildOneComponent },
+      { path: 'users', component: ChildTwoComponent }
+    ]
+  },
+  {
+    path: '',
+    redirectTo: '/parent',
+    pathMatch: 'full'
+  }
 ];
