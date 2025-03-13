892735d7ec94a0464b37d5cbd0b57ec44048e92a
UBSITSalango <20162396@s.ubaguio.edu>
2025-03-13 14:38:11 +0800

    Modifying the app.routes.ts and recreating the child components.

--- angular-child-routes-example/src/app/app.routes.ts
+++ angular-child-routes-example/src/app/app.routes.ts
@@ -1,3 +1,31 @@
 import { Routes } from '@angular/router';
+import { ParentComponent } from './parent/parent.component';
+import { ChildOneComponent } from './child-one/child-one.component';
+import { ChildTwoComponent } from './child-two/child-two.component';
+export const routes: Routes = [
+{
+path: 'parent',
+component: ParentComponent,
+children: [
+{
+path: 'child-one',
+component: ChildOneComponent
+},
+{
+path: 'child-two',
+component: ChildTwoComponent
+},
+{
+path: '',
+redirectTo: 'child-one',
+pathMatch: 'full'
+}
+]
+},
+{
+path: '',
 
-export const routes: Routes = [];
+redirectTo: '/parent',
+pathMatch: 'full'
+}
+];
