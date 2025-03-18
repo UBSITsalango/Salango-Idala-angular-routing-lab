a8b90f772eb519d48bfa17aff9498e6b85681b87
UBSITSalango <20162396@s.ubaguio.edu>
2025-03-18 15:55:46 +0800

    Committing the activities done for interpolation, property binding, event binding, and two-way binding.

--- angular-child-routes-example/src/app/app.component.ts
+++ angular-child-routes-example/src/app/app.component.ts
@@ -1,12 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-child-routes-example';
+  pageTitle = 'Angular 19 Data Binding';
+  currentTime = new Date();
+  imageUrl = 'https://images.unsplash.com/photo-1495360010541-f48722b34f7d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2F0fGVufDB8fDB8fHww';
+  isDisabled = true;
+  submitted = false;
+
+  clickCount = 0;
+
+  incrementCount() {
+    this.clickCount++;
+  }
+
+  onInput(event: Event) {
+    console.log((event.target as HTMLInputElement).value)
+  }
+
+  name='';
+  email='';
+  age='';
+  city='';
+  hobby='';
+
+  submitForm() {
+    this.submitted = true;
+  }
 }
