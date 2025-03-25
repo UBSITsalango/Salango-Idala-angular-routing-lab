47d816a1a344190a248c4627097a725aa6f92f86
UBSIT-Idala <20233614@s.ubaguio.edu>
2025-03-25 15:28:17 +0800

    Creating a temporary product form design.

--- product-management-app/src/app/components/product-form/product-form.component.ts
+++ product-management-app/src/app/components/product-form/product-form.component.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product.service';
+import { Product } from '../../models/product.model';
+import { CommonModule, NgClass, NgForOf, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-product-form',
-  imports: [],
   templateUrl: './product-form.component.html',
-  styleUrl: './product-form.component.css'
+  styleUrls: ['./product-form.component.css'],
+  imports: [NgClass, NgForOf, NgIf, CommonModule, FormsModule]
 })
-export class ProductFormComponent {
+export class ProductFormComponent implements OnInit {
+  product: Product = {
+    id: 0,
+    name: '',
+    category: '',
+    price: 0,
+    description: '',
+    quantity: 0,
+    imageUrl: '',
+    isAvailable: true,
+    rating: 0
+  };
+  isEditMode = false;
+  isSubmitting = false;
 
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+    if (productId) {
+      this.isEditMode = true;
+      this.productService.getProduct(Number(productId)).subscribe((product: Product | undefined) => {
+        if (product) {
+          this.product = product;
+        }
+      });
+    }
+  }
+
+  onSubmit(): void {
+    this.isSubmitting = true;
+
+    if (this.isEditMode) {
+      this.productService.updateProduct(this.product).subscribe(() => {
+        this.router.navigate(['/products']);
+      });
+    } else {
+      this.productService.addProduct(this.product).subscribe(() => {
+        this.router.navigate(['/products']);
+      });
+    }
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products']);
+  }
 }
