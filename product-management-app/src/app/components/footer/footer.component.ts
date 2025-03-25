f7076bdd0ccb6af4b6794e1d5d7c141d3effa1dc
UBSITSalango <20162396@s.ubaguio.edu>
2025-03-25 15:51:03 +0800

    Updating the footer component and added a route to dashboard.

--- product-management-app/src/app/components/footer/footer.component.ts
+++ product-management-app/src/app/components/footer/footer.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule, NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [NgClass, NgFor, NgIf, CommonModule, FormsModule, RouterLink, RouterLinkActive],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
-
+  currentYear: number = new Date().getFullYear();
 }
